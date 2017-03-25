58ee2dad431bfce4d7d5600e13398888ad8ef0b2
Tyler Lam <tylerlam888@gmail.com>
2017-03-25 16:41:52 -0400

    configured SES to send emails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,9 @@
 class PagesController < ApplicationController 
-	def index
-		@contact = Contact.new
+
+	def send_email
+		MyMailer.send_email(name: params[:name], email: params[:email], message: params[:message]).deliver
+		flash[:success] = "Message sent!"
+		redirect_to root_url
+
 	end
-
-	def create 
-	    @contact = Contact.new(contact_params)
-	    if @contact.save
-	    	name = params[:contact][:name]
-	    	email = params[:contact][:email]
-	    	body = params[:contact][:comments]
-
-	    	ContactMailer.contact_email(name, email, body).deliver
-
-	    	flash[:success] = 'Message sent.'
-	    	redirect_to pages_path
-	    else
-	    	flash[:danger] = 'Error occured, message has not been sent.'
-	      	redirect_to pages_path
-  		end
-	end
-
-	private 
-		def contact_params 
-			params.require(:contact).permit(:name, :email, :comments)
-		end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.perform_deliveries = true 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -51,4 +52,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.smtp_settings = {
+    :address => "email-smtp.us-west-2.amazonaws.com",
+    :port => 587,
+    :user_name => "A***REMOVED***", #Your SMTP user
+    :password => "***REMOVED***", #Your SMTP password
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
 end
