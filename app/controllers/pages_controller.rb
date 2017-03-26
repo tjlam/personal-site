55dfda8343dd79420387fa8660c553c64c840f19
Tyler Lam <tylerlam888@gmail.com>
2017-03-26 14:05:04 -0400

    added contacts data base

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,27 @@
 class PagesController < ApplicationController 
-
-	def send_email
-		MyMailer.send_email(name: params[:name], email: params[:email], message: params[:message]).deliver
-		flash[:success] = "Message sent!"
-		redirect_to root_url
-
+	def index
+		@contact = Contact.new
 	end
+
+	def create 
+		@contact = Contact.new(contact_params)
+
+		if @contact.save 
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			message = params[:contact][:message]
+
+			MyMailer.send_email(name, email, message).deliver 
+			flash[:success] = "Message sent!"
+			redirect_to root_url
+		else 
+			flash[:danger] = "An error has occured..."
+			redirect_to root_url
+		end
+	end
+
+	private 
+		def contact_params 
+			params.require(:contact).permit(:name, :email, :message)
+		end
 end
--- db/migrate/20170313021224_create_contacts.rb
+++ db/migrate/20170313021224_create_contacts.rb
@@ -3,7 +3,7 @@ class CreateContacts < ActiveRecord::Migration[5.0]
     create_table :contacts do |t|
     	t.string :name 
     	t.string :email 
-    	t.string :comments
+    	t.string :message
 
     	t.timestamps
     end
